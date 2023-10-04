db7f4c728f94ede672189214bc9ac048473b43f4
Kevin <kevinolongo9@gmail.com>
2023-10-04 17:49:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ if we reach the middle, and all the letters match, return true
 */
 
 /*
-  Add written explanation of your solution here
+           //Add written explanation of your solution here
+
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  const j = word.length - 1 - i;
+  if (word[i] !== word[j]) {
+  // if the letters don't match, return false
+  return false;
+}
+}
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
+
 */
 
 // You can run `node index.js` to view these console logs
