a39c4a8f2c6b86cfee4201bc18e50c3f9aad2b20
Kevin <kevinolongo9@gmail.com>
2023-10-04 17:42:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  const j = word.length - 1 - i;
+  if (word[i] !== word[j]) {
+  // if the letters don't match, return false
+  return false;
+}
+}
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+     //Add your pseudocode here
+
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
@@ -17,6 +36,27 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
